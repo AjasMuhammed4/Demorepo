10a52180ae0e711acbff07a8ff1058aac3fb7a52
AjasMuhammed4 <137448035+AjasMuhammed4@users.noreply.github.com>
2023-06-25 23:22:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Sample deploy
+          Sample  deploy
         </a>
       </header>
     </div>
