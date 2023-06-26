cd46abe8e62d5e6570067ea817aeb635950747b4
AjasMuhammed4 <ajaskmj4@gmail.com>
2023-06-27 00:08:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          The is example for dockerize conatiner
         </a>
       </header>
     </div>
