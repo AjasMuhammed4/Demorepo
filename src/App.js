c7baf845819ffb3758d35a8aa2a42a03819a8c31
AjasMuhammed4 <ajaskmj4@gmail.com>
2023-06-27 00:20:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is an example of docker image
         </a>
       </header>
     </div>
